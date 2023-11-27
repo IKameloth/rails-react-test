3c23ccc2f187364fc168dfda1bcf4b6e21438561
Camilo Matteo <camilomatteo@iMac-de-Camilo.local>
2023-11-27 18:18:22 -0300

    🔧 Add reactjs config

---
+++ app/javascript/App.jsx
@@ -0,0 +1,6 @@
+import * as React from "react"
+import { render } from "react-dom"
+import { Home } from "./views/Home"
+
+const rootElement = document.getElementById("root")
+render(<Home />, rootElement)
---
+++ app/javascript/components/places_list.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const PlaceLists = () => {
+  return (
+    <div>Places list rendered from react</div>
+  )
+}
+
+export default PlaceLists
+
